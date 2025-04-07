d8d422fce04ce2c20baf9083800b72da69960c9e
victoriaferrario <125160421+victoriaferrario@users.noreply.github.com>
2025-04-07 13:56:15 -0300

    Update guia3.hs
    hasta el 7 :3

--- guia3.hs
+++ guia3.hs
@@ -13,6 +13,12 @@ f n | n == 1 = 8
     | n == 4 = 131
     | n == 16 = 16
 
+-- sin pattern matching
+fAlt :: Integer -> Integer
+f 1 = 0 
+f 4 = 131
+f 16 = 15
+
 -- b)
 g :: Integer -> Integer 
 g n | n == 8 = 16
@@ -32,9 +38,8 @@ k n = g x
 -- EJERCICIO 2 -----------------------------------------
 -- a) Valor absoluto
 -- Importante: en terminal usar paréntesis (-n)
-absolute :: Integer -> Integer 
-absolute n 
-        | n < 0 = -n
+abs :: Integer -> Integer 
+abs n   | n < 0 = -n
         | otherwise = n
 
 -- b) maximo Absoluto, devuelve el máximo de valor absoluto entre 2
@@ -99,6 +104,7 @@ esMultiploDe x y = mod x y == 0
 -- i) digitoUnidades dado un número entero, extrae su dígito de las unidades 
 ---- función recursiva 
 ---- [div] devuelve cuantas veces el primer numero puede ser dividido por el segundo
+---- entendí mal solo pide el utlimo digito se hace con div x 10 
 digitoUnidades :: Integer -> Integer 
 digitoUnidades x 
     | x > 0 = 1 + digitoUnidades(div x 10)
@@ -110,4 +116,114 @@ digitoDecenas :: Integer -> Integer
 digitoDecenas x
     | x > 9 = digitoUnidades (div x 10)
     | otherwise = 0
+
+
+ype Punto2D = (Float, Float)
+
+-- Ejercicio 4. ⋆ Especificar e implementar las siguientes funciones utilizando tuplas para representar pares y ternas de
+-- n´umeros.
+
+-- a) productoInterno: calcula el producto interno entre dos tuplas de R × R.
+
+productoInterno :: Punto2D -> Punto2D -> Float
+productoInterno a b = fst a * fst b + snd a * snd b
+
+-- b) esParMenor: dadas dos tuplas de R × R, decide si cada coordenada de la primera tupla es menor a la coordenada
+esParmenor :: Punto2D -> Punto2D -> Bool
+esParmenor a b = fst a  < fst b && snd a < snd b
+
+-- c) distancia: calcula la distancia eucl´ıdea entre dos puntos de R2. 
+------ Modulo de la resta entre los vectores
+distancia :: Punto2D -> Punto2D -> Float
+distancia a b = sqrt((fst a - fst b)**2 + (snd a- snd b)**2)
+
+
+-- pwd donde esta ls todo cd abre carpeta
+
+-- d) sumaTerna: dada una terna de enteros, calcula la suma de sus tres elementos.
+sumaTerna :: (Integer, Integer, Integer) -> Integer
+sumaTerna (x,y,z) = x+y+z
+
+-- e) sumarSoloMultiplos: dada una terna de n´umeros enteros y un natural, calcula la suma de los elementos de la terna que
+-- son m´ultiplos del n´umero natural.
+-- Por ejemplo:
+-- sumarSoloMultiplos (10,-8,-5) 2 ⇝ 2
+-- sumarSoloMultiplos (66,21,4) 5 ⇝ 0
+-- sumarSoloMultiplos (-30,2,12) 3 ⇝ -18
+
+-- precondición n = Natural
+sumarSoloMultiplos :: (Integer, Integer, Integer) -> Integer -> Integer
+sumarSoloMultiplos (x, y, z) n 
+    | esMultiploDe x n && esMultiploDe y n && esMultiploDe z n = x + y + z
+    | esMultiploDe x n && esMultiploDe y n = x + y
+    | esMultiploDe y n && esMultiploDe z n = y + z 
+    | esMultiploDe x n && esMultiploDe z n = x + z
+    | esMultiploDe x n = x
+    | esMultiploDe y n = y
+    | esMultiploDe z n = z
+    | otherwise = 0
+
+-- f) posPrimerPar: dada una terna de enteros, devuelve la posici´on del primer n´umero par si es que hay alguno, o devuelve
+-- 4 si son todos impares.
+posPrimerPar ::(Integer, Integer, Integer) -> Integer
+posPrimerPar (x, y, z)
+    | esMultiploDe x 2 = 1
+    | esMultiploDe y 2 = 2
+    | esMultiploDe z 2 = 3
+    | otherwise = 4
+
+-- g) crearPar :: a -> b -> (a, b): a partir de dos componentes, crea un par con esos valores. Debe funcionar para ele-
+-- mentos de cualquier tipo.
+crearPar :: a -> b -> (a,b)
+crearPar a b = (a,b)
+
+-- h) invertir :: (a, b) -> (b, a): invierte los elementos del par pasado como par´ametro. Debe funcionar para elementos
+-- de cualquier tipo.
+invertir :: (a,b) -> (b,a)
+invertir (a,b) = (b,a)
+
+-- i) Reescribir los ejercicios productoInterno, esParMenor y distancia usando el siguiente renombre de tipos:
+-- type Punto2D = (Float, Float)
+
+
+
+-- Ejercicio 5. Implementar la funci´on todosMenores :: (Integer, Integer, Integer) -> Bool
+f :: Integer -> Integer 
+f n | n <= 7 = n * n 
+    | n > 7 = 2*n - 1
+
+g :: Integer -> Integer 
+g n | esMultiploDe n 2 = div n 2
+    | otherwise = 3*n + 1
+
+todosMenores :: (Integer, Integer, Integer) -> Bool
+todosMenores (x,y,z) = (f x > g x) && (f y > g y) && (f z > g z)
+
+-- Ejercicio 6. 
+
+type Anio = Integer
+type EsBisiesto = Bool
+
+bisiesto :: Anio -> EsBisiesto
+bisiesto a = not(not(esMultiploDe a 4) || (esMultiploDe a 100  && not(esMultiploDe a 400)))
+-- el not al principio --> la especificacion define res = false 
+
+
+-- Ejercicio 7.
+-- a) Implementar la funci´on:
+-- distanciaManhattan:: (Float, Float, Float) -> (Float, Float, Float) -> Float
+-- problema distanciaManhattan (p : R × R × R, q : R × R × R) : R {
+-- requiere: {T rue}
+-- asegura: {res = P2
+-- i=0 |pi − qi|}
+-- }
+-- Por ejemplo:
+-- distanciaManhattan (2, 3, 4) (7, 3, 8) ⇝ 9
+-- distanciaManhattan ((-1), 0, (-8.5)) (3.3, 4, (-4)) ⇝ 12.8
+
+distanciaManhattan :: (Float, Float, Float) -> (Float, Float, Float) -> Float
+distanciaManhattan (a,b,c) (d,e,f) = (abs (a-d) + abs(b-e) + abs(c-f))
+
+
+-- b) Reimplementar la funci´on teniendo en cuenta el siguiente tipo: type Punto3D = (Float, Float, Float)
 
