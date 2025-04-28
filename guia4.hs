ebf0f041530c5ad05b93dfbd1ba7f624aa634eac
victoriaferrario <125160421+victoriaferrario@users.noreply.github.com>
2025-04-28 10:01:54 -0300

    preguntar por ejercicio 14 y 15 (llegue hasta el 16)

--- guia4.hs
+++ guia4.hs
@@ -1,3 +1,6 @@
+-- Guia 4
+
+
 factorial :: Integer -> Integer
 -- signatura de la función Integer -> Integer
 factorial n
@@ -54,13 +57,151 @@ todosDigitosIguales n
     | otherwise = False
 
 --Ejercicio 7 
+--- lo poseyo el diablo 
 iesimoDigito :: Integer -> Integer -> Integer 
--- n >= 0 
--- cantdigitos(n) > i >= 1
-iesimoDigito n 1 = div n 10^(cantDigitos n -1)
-iesimoDigito n i = iesimoDigito (mod n 10^cantDigitos(n-1)) (i-1)
+-- Primer integer: n   n >= 0 
+-- Segundo integer: i  cantdigitos(n) > i >= 1
+iesimoDigito n 1 = div n (10^(cantDigitos n - 1))
+iesimoDigito n i = iesimoDigito (sinprimerdigito n) (i - 1)
 
+sinprimerdigito :: Integer -> Integer 
+sinprimerdigito n = mod n (10^(cantDigitos n - 1)) 
 
 cantDigitos :: Integer -> Integer
 cantDigitos 0 = 0
 cantDigitos n = 1 + cantDigitos(div n 10)
+
+
+--Ejercicio 8 
+sumaDigitos :: Integer -> Integer 
+sumaDigitos n 
+    | cantDigitos n == 1 = primerDigito
+    | cantDigitos n > 1 = primerDigito + sumaDigitos(sinprimerdigito n)
+            where primerDigito = iesimoDigito n 1
+
+--Ejercicio 9 
+esCapicua :: Integer -> Bool 
+esCapicua n 
+    | cantDigitos n == 0 = True 
+    -- Comparo primer y ultimo dígito
+    | iesimoDigito n 1 == iesimoDigito n (cantDigitos n) = esCapicua sinprimeryultimo
+    | otherwise = False 
+            where sinprimeryultimo = div (sinprimerdigito n) 10
+
+
+-- Corrección 10201 si bien es capicua mod 10201 (10^(4)) = 201 no 0201
+-- Manera más rigurosa de hacerlo = implementar funcion de invertir y luego comparar con numero original 
+digitoUnidades :: Integer -> Integer 
+digitoUnidades x = mod x 10
+
+sacarUltimoDigito :: Integer -> Integer 
+sacarUltimoDigito n 
+    | n < 0 = -sacarUltimoDigito (-n)
+    | otherwise = div n 10
+
+invertir :: Integer -> Integer 
+invertir n
+    | n < 0 = - invertir (-n)
+    | n < 10 = n
+    | otherwise = ultimodigitoaprimero + invertir (sacarUltimoDigito n)
+        where ultimodigitoaprimero = digitoUnidades n * (10^(cantDigitos n - 1))
+
+
+esCapicuaBien :: Integer -> Bool 
+esCapicuaBien n = n == invertir n 
+
+------------------------------------------------------------
+
+--Ejercicio 10
+--- a 
+diezasumatoria :: Integer -> Integer 
+-- 2^0 = 1 caso base
+diezasumatoria 0 = 1
+diezasumatoria n = 2^n + diezasumatoria ( n - 1 )
+
+---- serie geométrica debería dar lo mismo que 
+diezaformula :: Integer -> Integer 
+diezaformula n = div (1 - 2^(n+1)) ( 1 - 2 )
+
+--- b
+diezbsumatoria :: Integer -> Integer -> Integer 
+diezbsumatoria 1 q = q 
+diezbsumatoria n q = q ^ n + diezbsumatoria ( n - 1 ) q 
+
+--Ejercicio 11
+--a 
+--- Importante el fromIntegral que transforma de un int a un float del preludio
+eAprox :: Integer -> Float 
+eAprox 0 = 1
+eAprox n = fromIntegral (div 1 (factorial n)) + eAprox ( n - 1 )
+
+--Ejercicio 12
+raizDe2Aprox :: Integer -> Float 
+raizDe2Aprox n = sucesion12 n - 1
+
+sucesion12 :: Integer -> Float 
+sucesion12 1 = 2
+sucesion12 n = 2 + (1 / sucesion12 (n-1))
+
+--Ejercicio 13
+--- Sumatoria dentro de sumatoria, son como 10 b
+sumatoriatrece :: Integer -> Integer -> Integer
+sumatoriatrece n m 
+    | n == 1 = 1 * m 
+    | otherwise = diezbsumatoria m n + sumatoriatrece (n-1) m
+
+--Ejercicio 14 
+sumaPotencias :: Integer -> Integer -> Integer -> Integer
+sumaPotencias q n m 
+    -- caso base 
+    | m == 0 = 0 
+    | otherwise = (q ^ m) * diezbsumatoria n q + sumaPotencias q n (m - 1)
+    -- sumatoria de a 
+
+--Ejercicio 15 
+----- Suma de gauss =  1 + 2 + 3 + ...... + n
+------- Se puede no hacer por recursión con lo siguiente 
+sumaGauss :: Integer -> Integer 
+sumaGauss n = div (n*(n+1)) 2
+
+sumaRacionales :: Integer -> Integer -> Float 
+sumaRacionales n m 
+    | m == 1 = int sumadep
+    | otherwise = (int sumadep / int m) + sumaRacionales n (m - 1)
+        where 
+            sumadep = sumaGauss n
+            int = fromIntegral
+
+--Ejercicio 16 
+
+---- (a)
+menorDivisor :: Integer -> Integer 
+menorDivisor 1 = 1
+menorDivisor n = menorDivisorDesde n 2
+-- precond = n > 1
+menorDivisorDesde :: Integer -> Integer -> Integer
+menorDivisorDesde n k
+    | mod n k == 0 = k
+    | otherwise = menorDivisorDesde n (k+1)
+
+---- (b)
+esPrimo :: Integer -> Bool 
+esPrimo x = menorDivisor x == x
+
+---- (c) --> Sino tienen algún divisor en común
+sonCoprimos :: Integer -> Integer -> Bool 
+sonCoprimos n m = (mod n (menorDivisor m) == 0) || (mod m (menorDivisor n) == 0 )
+
+---- (d)
+nEsimoPrimo :: Integer -> Integer 
+nEsimoPrimo 1 = 2
+nEsimoPrimo n = proximoprimo (nEsimoPrimo (n - 1))
+
+proximoprimo :: Integer -> Integer 
+proximoprimo 1 = 2 
+proximoprimo n 
+    | esPrimo (n+1) = n + 1
+    | otherwise = proximoprimo (n + 1)
+
+--Ejercicio 19 
+esSumaInicialDePrimos :: Integer -> Bool 
